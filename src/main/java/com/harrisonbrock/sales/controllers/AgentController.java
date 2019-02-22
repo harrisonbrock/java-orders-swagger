30b26e3d82a9d2ef21b862b25bbf968950853502
Harrison Brock <harrison@harrisonbrock.com>
2019-02-21 22:56:51 -0500

    added route to get agents by id

--- src/main/java/com/harrisonbrock/sales/controllers/AgentController.java
+++ src/main/java/com/harrisonbrock/sales/controllers/AgentController.java
@@ -4,10 +4,13 @@ import com.harrisonbrock.sales.models.Agent;
 import com.harrisonbrock.sales.repositories.AgentRepository;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/agents", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -22,6 +25,19 @@ public class AgentController {
     @GetMapping()
     public List<Agent> getAllAgents(){
         return  repository.findAll();
+    }
+
+    @GetMapping("/agentcode/{id}")
+    public List<Agent> getAgentById(@PathVariable long id) {
+        Optional<Agent> agents = repository.findById(id);
+
+        if (agents.isPresent()) {
+            return agents.stream().collect(Collectors.toList());
+        }
+        else {
+            return agents.stream().collect(Collectors.toList());
+        }
+
     }
 
 }
